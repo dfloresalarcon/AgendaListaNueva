635ffc504df688aa2ce28915275b9b772ba53973
dfloresalarcon <dfloresalarcon@danielcastelao.org>
2014-12-03 11:09:48 +0100

    1 Commit Rama Nueva Añadida clase Personas

---
+++ AgendaListaNueva/AgendaListaCompleta/app/src/main/java/com/example/dfloresalarcon/agendalistanueva/Personas.java
@@ -0,0 +1,16 @@
+package com.example.dfloresalarcon.agendalistanueva;
+
+/**
+ * Created by dfloresalarcon on 3/12/14.
+ */
+public class Personas {
+
+    String n;
+    String t;
+
+    public Personas(String n, String t)
+    {
+
+    }
+    public Personas(){}
+}
